7eca405af015fb9c2d45817d63d1a3ea552ce521
Aboubakr Elbouzidi <aboubakerelbouzidi@gmail.com>
2023-03-17 23:35:40 +0100

    nav-bar and hero

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -5,7 +5,7 @@ export default function Hero() {
       <>
       <section className='hero'>
       <img className='group-images' src={groupImage} alt="Images" />
-      <h1 className='title'>Online Experiences</h1>
+      <h1 className='header'>Online Experiences</h1>
       <p className='text'>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.
         </p>
       </section>         
